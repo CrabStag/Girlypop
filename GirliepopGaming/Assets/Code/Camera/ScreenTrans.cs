27319b3586720d57b673b7f4d262e92fda4b7053
Jacco Verhoeckx <jaccozakelijk@hotmail.com>
2024-12-15 17:56:01 +0100

    Added simple sound effect script to be added to clickable objects

--- GirliepopGaming/Assets/Code/Camera/ScreenTrans.cs
+++ GirliepopGaming/Assets/Code/Camera/ScreenTrans.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class ScreenTrans : MonoBehaviour
 {
     public Transform targetScreen;
-    public AudioClip transitionSoundClip;
-    public AudioSource buttonSoundSource;
 
     private void OnMouseUpAsButton()
     {
         Camera.main.transform.position = targetScreen.position;
-
-        buttonSoundSource.clip = transitionSoundClip;
-        buttonSoundSource.Play();
     }
 }
 
---
+++ GirliepopGaming/Assets/Code/SoundEffectOnClick.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectOnClick : MonoBehaviour
+{
+    public AudioClip transitionSoundClip;
+
+    [Range(0, 100)]
+    public float volume = 100f;
+
+    private AudioSource buttonSoundSource;
+
+    private void Start()
+    {
+        buttonSoundSource = Camera.main.GetComponentInChildren<AudioSource>();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        buttonSoundSource.clip = transitionSoundClip;
+        buttonSoundSource.volume = volume / 100;
+        buttonSoundSource.Play();
+    }
+}
